16ec332c1c8935f03d28ed4fe7ca22d78943bda2
MarioMarcello98 <marmarc98@gmail.com>
2024-12-08 01:14:52 +0100

    Aggiunto proportion più modifiche da controllare

--- src/main/java/retrievers/GetJiraInfo.java
+++ src/main/java/retrievers/GetJiraInfo.java
@@ -1,42 +1,79 @@
 package retrievers;
 
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 public class GetJiraInfo {
-    public static int numIssues;
-    public static ArrayList<String> issuesKeys;
-    // https://issues.apache.org/jira/rest/api/2/search?jql=project=bookkeeper+and+type=bug+and+(status=closed+or+status=resolved)+and+resolution=fixed
-    public static void main(String[] args) throws JSONException, IOException {
-        String projectName = "BOOKKEEPER";
-        String query = "search?jql=project=" + projectName + "+and+type=bug+and+(status=closed+or+status=resolved)+and+resolution=fixed&maxResults=500";
-        String url = "https://issues.apache.org/jira/rest/api/2/" + query;
+    public static HashMap<LocalDateTime, String> releaseNames;
+    public static HashMap<LocalDateTime, String> releaseID;
+    public static ArrayList<LocalDateTime> releases;
+    public static Integer numVersions;
+    public static ArrayList<LocalDateTime> getReleaseInfo(String projName) throws IOException, JSONException {
+        //Fills the arraylist with releases dates and orders them
+        //Ignores releases with missing dates
+        releases = new ArrayList<LocalDateTime>();
+        int i;
+        String url = "https://issues.apache.org/jira/rest/api/2/project/" + projName;
         JSONObject json = readJsonFromUrl(url);
-        JSONArray issues = json.getJSONArray("issues");
-        issuesKeys = new ArrayList<>();
-        System.out.println(issues.length());
-        for (int i = 0; i < issues.length(); i++) {
-            String key = "";
-            if (issues.getJSONObject(i).has("key")) {
-                issuesKeys.add(issues.getJSONObject(i).get("key").toString());
+        JSONArray versions = json.getJSONArray("versions");
+        releaseNames = new HashMap<LocalDateTime, String>();
+        releaseID = new HashMap<LocalDateTime, String> ();
+        for (i = 0; i < versions.length(); i++ ) {
+            System.out.println(versions.getJSONObject(i));
+            String name = "";
+            String id = "";
+            if(versions.getJSONObject(i).has("releaseDate")) {
+                if (versions.getJSONObject(i).has("name"))
+                    name = versions.getJSONObject(i).get("name").toString();
+                if (versions.getJSONObject(i).has("id"))
+                    id = versions.getJSONObject(i).get("id").toString();
+                addRelease(versions.getJSONObject(i).get("releaseDate").toString(),
+                        name,id);
             }
         }
-        System.out.println(url);
-        System.out.println(issuesKeys);
+        // order releases by date
+        Collections.sort(releases, new Comparator<LocalDateTime>(){
+            //@Override
+            public int compare(LocalDateTime o1, LocalDateTime o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return releases;
     }
-    public static void getFields(JSONArray issues) {
+    public static void addRelease(String strDate, String name, String id) {
+        LocalDate date = LocalDate.parse(strDate);
+        LocalDateTime dateTime = date.atStartOfDay();
+        if (!releases.contains(dateTime))
+            releases.add(dateTime);
+        releaseNames.put(dateTime, name);
+        releaseID.put(dateTime, id);
     }
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
         InputStream is = new URL(url).openStream();
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             String jsonText = readAll(rd);
-            return new JSONObject(jsonText);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
         } finally {
             is.close();
         }
@@ -50,4 +87,3 @@ public class GetJiraInfo {
         return sb.toString();
     }
 }
-
--- src/main/java/retrievers/GetTicketInfo.java
+++ src/main/java/retrievers/GetTicketInfo.java
@@ -1,26 +1,38 @@
+package retrievers;
+
 import entities.Ticket;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import retrievers.GetJiraInfo;
+
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
-public class GetTicketInfo {
+import java.io.*;
+import java.net.URL;
+
+public class TicketRetriever {
     public static ArrayList<Ticket> tickets;
-    public static HashSet<LocalDateTime> allVersionsHashSet = new HashSet<>();
-    public static List<LocalDateTime> allVersions = new ArrayList<>();
-    // https://issues.apache.org/jira/rest/api/2/search?jql=project=bookkeeper+and+type=bug+and+(status=closed+or+status=resolved)+and+resolution=fixed
+    public static ArrayList<LocalDateTime> versions = new ArrayList<>();
+
     public static void main(String[] args) throws JSONException, IOException {
         String projectName = "BOOKKEEPER";
-        String query = "search?jql=project=" + projectName + "+and+type=bug+and+(status=closed+or+status=resolved)+and+resolution=fixed&maxResults=500";
+        String query = "search?jql=project=" + projectName + "+and+type=bug+and+(status=closed+or+status=resolved)+and+resolution=fixed&maxResults=1000";
         String url = "https://issues.apache.org/jira/rest/api/2/" + query;
+
         JSONObject json = readJsonFromUrl(url);
         JSONArray issues = json.getJSONArray("issues");
         System.out.println(issues.length());
+
         tickets = new ArrayList<>();
+
+        versions = GetJiraInfo.GetJiraInfo(projectName);
+        System.out.println(versions);
         for (int i = 0; i < issues.length(); i++) {
             try {
                 tickets.add(getTicket(issues.getJSONObject(i)));
@@ -28,57 +40,111 @@ public class GetTicketInfo {
                 e.printStackTrace();
             }
         }
-        System.out.println(url);
         /*
         int x = 0;
         for (Ticket ticket : tickets) {
             System.out.println(ticket.id);
             System.out.println(ticket.key);
             System.out.println(ticket.affectedVersions);
-            System.out.println(ticket.resolutionDate);
+            System.out.println(ticket.openingVersion);
             System.out.println(ticket.injectedVersion);
+            System.out.println(ticket.fixVersion);
             System.out.println('\n');
             if (!ticket.affectedVersions.isEmpty())
                 x++;
         }
         System.out.println(x);
          */
-        System.out.println(allVersionsHashSet);
-        allVersions.addAll(allVersionsHashSet);
-        Collections.sort(allVersions);
-        System.out.println(allVersions);
+        System.out.println("proportion mean: " + getProportionMean());
+        Proportion.estimateInjectedVersion(tickets, getProportionMean());
+        for (Ticket ticket : tickets) {
+            System.out.println("id: " + ticket.id);
+            System.out.println(ticket.key);
+            System.out.println("Affected versions: " + ticket.affectedVersions);
+            System.out.println("Opening version: " + ticket.openingVersion);
+            System.out.println("Injected version: " + ticket.injectedVersion);
+            System.out.println("Fix version: " + ticket.fixVersion);
+            System.out.println('\n');
+        }
     }
+
     public static Ticket getTicket(JSONObject ticketInfo) throws JSONException {
+
         Ticket ticket = new Ticket();
+
         ticket.id = ticketInfo.get("id").toString();
         ticket.key = ticketInfo.get("key").toString();
+
+
         JSONObject fields = ticketInfo.getJSONObject("fields");
-        ticket.resolutionDate = LocalDateTime.parse(fields.get("resolutiondate").toString().substring(0, 21));
-        ticket.creationDate = LocalDateTime.parse(fields.get("created").toString().substring(0, 21));
+
+
+        LocalDateTime resolutionDate = LocalDateTime.parse(fields.get("resolutiondate").toString().substring(0, 21));
+        ticket.fixVersion = getVersion(resolutionDate);
+        LocalDateTime creationDate = LocalDateTime.parse(fields.get("created").toString().substring(0, 21));
+        ticket.openingVersion = getVersion(creationDate);
+
+
+
         JSONArray versions = fields.getJSONArray("versions");
+
         String name = "";
-        HashMap<String, LocalDateTime> injectedVersion = new HashMap<>();
-        injectedVersion.put(name, ticket.resolutionDate);
+
+        ArrayList<LocalDateTime> injectedVersion = new ArrayList<>();
+        injectedVersion.add(resolutionDate);
+
         for (int i = 0; i < versions.length(); i++) {
             JSONObject version = versions.getJSONObject(i);
+
             LocalDate releaseDate = LocalDate.parse(version.get("releaseDate").toString());
             LocalDateTime releaseDateTime = releaseDate.atStartOfDay();
-            if (releaseDateTime.isBefore(ticket.resolutionDate)) {
-                ticket.affectedVersions.put(version.get("name").toString(), releaseDateTime);
-                if (injectedVersion.get(name).isAfter(releaseDateTime)) {
-                    injectedVersion.remove(name);
-                    name = version.get("name").toString();
-                    injectedVersion.put(name, releaseDateTime);
-                    allVersionsHashSet.add(injectedVersion.get(name));
+
+
+                if (releaseDateTime.isBefore(resolutionDate)) {
+                    ticket.affectedVersions.add(getVersion(releaseDateTime));
                 }
             }
+
+
+                if (!ticket.affectedVersions.isEmpty()) {
+                    Collections.sort(ticket.affectedVersions);
+                    ticket.injectedVersion = ticket.affectedVersions.get(0);
+                    computeProportion(ticket);
+                }
+
+
+            return ticket;
         }
-        if (!injectedVersion.containsKey("")) {
-            ticket.injectedVersion = injectedVersion;
-            ticket.proportion =
+public static float getProportionMean() {
+    float sum = 0;
+    int count = 0;
+    for (Ticket ticket : tickets) {
+        if (ticket.proportion > 0) {
+            sum += ticket.proportion;
+            count++;
         }
-        return ticket;
     }
+        return sum/count;
+    }
+
+    private static int getVersion(LocalDateTime date) {
+        int i = 0;
+        while (versions.get(i).isBefore(date)) {
+            i++;
+        }
+        return i;
+    }
+    /*
+     *   proportion = (fixVersion - injectedVersion)/(fixVersion - openingVersion)
+     */
+    public static void computeProportion(Ticket ticket) {
+
+        if (ticket.fixVersion > ticket.openingVersion && ticket.fixVersion > ticket.injectedVersion && ticket.injectedVersion < ticket.openingVersion) {
+            ticket.proportion = (float)(ticket.fixVersion - ticket.injectedVersion)/(ticket.fixVersion - ticket.openingVersion);
+            System.out.println(ticket.proportion);
+        }
+    }
+
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
         try (InputStream is = new URL(url).openStream()) {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
@@ -86,6 +152,7 @@ public class GetTicketInfo {
             return new JSONObject(jsonText);
         }
     }
+
     private static String readAll(Reader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
         int cp;
@@ -94,4 +161,5 @@ public class GetTicketInfo {
         }
         return sb.toString();
     }
+
 }
